af2a3fb2246b35a9d0f6d986c07b0179b65a2d06
krowik <krowikgaming@gmail.com>
2026-02-14 21:57:14 +0100

    Splitted get_table logic between Scraper and ScrapingManager.

--- scraper_class.py
+++ scraper_class.py
@@ -1,5 +1,4 @@
 import io
-import json
 import os
 import re
 from collections import Counter
@@ -17,14 +16,15 @@ class Scraper:
 
     def __init__(self, wiki_url, phrase, use_local_html_file_instead=False):
         """
-
-        :param wiki_url: URL link to the wiki which will be scraped from
+        :param wiki_url: URL link to the wiki (or path to a local file)
+                         which will be scraped from
         :type wiki_url: any
 
         :param phrase: Title of the wiki article page which will be scraped
         :type phrase: str
 
-        :param use_local_html_file_instead: True/False if wiki_url is a path to a single local HTML file.
+        :param use_local_html_file_instead: True/False if wiki_url is a path to
+                                            a single local HTML file.
         :type use_local_html_file_instead: bool
         """
         # Check phrase
@@ -37,37 +37,42 @@ class Scraper:
                 raise FileNotFoundError(f"File {wiki_url} doesn't exist")
         self.phrase = phrase
         self.base_url = wiki_url
-        self.read_local_file = use_local_html_file_instead # Bool value
-        self.soup = None  # Page content will be loaded here as a BeautifulSoup instance
+        self.read_local_file = use_local_html_file_instead  # Bool value
+        self.soup = None  # Page content will be loaded as a BeautifulSoup inst
 
         if use_local_html_file_instead:
             self.exact_url = wiki_url
         else:
-            self.exact_url = f"{wiki_url}/{phrase.replace(" ", '_')}"
+            self.exact_url = f"{wiki_url}/{phrase.replace(' ', '_')}"
 
     def get_children_phrases(self):
         """
-        Extracts and returns all relevant child phrases from the parsed HTML content.
-        :return: A list of strings, where each string represents the title of an article
-            linked from the current page, decoded and formatted. Returns `None` if the
-            required container is not present in the HTML content.
+        Extracts and returns all relevant child phrases from the parsed HTML
+        content.
+        :return: A list of strings, where each string represents the title of
+            an article linked from the current page, decoded and formatted.
+            Returns `None` if the required container is not present in the
+            HTML content.
         :rtype: list[str] | None
         """
         content = self.soup.find('div', class_='mw-parser-output')
         if not content:
             return None
         result_phrases = []
-        # Get all the links to other articles. We search for links which href starts with '/wiki/'
+        # Get all the links to other articles. We search for links which href
+        # starts with '/wiki/'
         for link in content.find_all('a', href=re.compile('^/wiki/')):
             href = link['href']
             # Get the title of the article that the link leads to
-            # Some titles are percent encoded (e.g. Pokémon is encoded as Pok%C3%A9mon), and the original
-            # title can be decoded using the unquote function.
+            # Some titles are percent encoded (e.g. Pokémon is encoded as
+            # Pok%C3%A9mon), and the original title can be decoded using the
+            # unquote function.
             link_title = href.removeprefix('/wiki/')
             link_title = unquote(link_title)
             link_title = link_title.replace('_', ' ')
-            # Links to the actual wiki sites have a title parameter equal to link_title.
-            # If there's no such title for the link, it means that it isn't a relevant link (i.e., image)
+            # Links to the actual wiki sites have a title parameter equal to
+            # link_title. If there's no such title for the link, it means that
+            # it isn't a relevant link (i.e., image)
             if link.get('title'):
                 if link.get('title') == link_title:
                     result_phrases.append(link_title)
@@ -114,33 +119,40 @@ class Scraper:
 
     def fetch_data(self):
         """
-        Fetches data from either a local file or a remote URL depending on the `read_local_file` attribute. If
-        `read_local_file` is set to True, it tries to fetch data from a local file using
-        the `fetch_data_from_local_file` method. Otherwise, data is fetched from a remote
-        URL using the `fetch_data_from_wiki` method. If there was an error, ConnectionError is raised.
+        Fetches data from either a local file or a remote URL depending on the
+        `read_local_file` attribute. If `read_local_file` is set to True, it
+        tries to fetch data from a local file using the
+        `fetch_data_from_local_file` method. Otherwise, data is fetched from a
+        remote URL using the `fetch_data_from_wiki` method. If there was an
+        error, ConnectionError is raised.
 
-        :return: `True` if data is successfully fetched and the soup object is initialized.
+        :return: `True` if data is successfully fetched and the soup object is
+                 initialized.
         :rtype: bool
-        :raises ConnectionError: If data loading from local file or remote URL fails,
-            or if the soup object cannot be initialized.
+        :raises ConnectionError: If data loading from local file or remote URL
+            fails, or if the soup object cannot be initialized.
         """
         if self.read_local_file:
             if not self.fetch_data_from_local_file():
-                raise ConnectionError(f"Failed to load data from local file: {self.base_url}")
+                raise ConnectionError(f"Failed to load data from local file: "
+                                      f"{self.base_url}")
         else:
             if not self.fetch_data_from_wiki():
-                raise ConnectionError(f"Failed to load data from {self.exact_url}")
+                raise ConnectionError(f"Failed to load data from "
+                                      f"{self.exact_url}")
         if not self.soup:
             raise ConnectionError(f"Couldn't load soup from {self.exact_url}")
         return True
 
     def get_summary(self):
         """
-        Gets the first paragraph of an article corresponding with Scraper's phrase.
-        :return: String value of the first paragraph's text or None if nothing was found.
+        Gets the first paragraph of an article corresponding with Scraper's
+        phrase.
+        :return: String value of the first paragraph's text or None if nothing
+                 was found.
         """
         if not self.soup:
-         self.fetch_data()
+            self.fetch_data()
 
         # Get content of the div with "mw-parser-output" label
         content = self.soup.find('div', class_='mw-parser-output')
@@ -156,36 +168,41 @@ class Scraper:
         for p in paragraphs:
             text = p.get_text().strip()
             if text:
-                return text  # read text from paragraphs until the first non-null appears
+                # read text from paragraphs until the first non-null appears
+                return text
         return None
 
     def get_table(self, table_number, first_row_header=False):
         """
         Extracts a specific HTML table from a processed article and converts it
-        into a pandas DataFrame. Supports optionally treating the first row of the table as the
-        header for the resulting DataFrame.
+        into a pandas DataFrame. Supports optionally treating the first row of
+        the table as the header for the resulting DataFrame.
 
-        :param table_number: Specifies the table index to extract (1-based index).
+        :param table_number: Specifies the table index to extract (1-based
+                             index).
         :type table_number: int
 
-        :param first_row_header: Indicates whether the first row of the table should be used as the
-            DataFrame's header. If True, the first row is used as column names; otherwise, no column
-            headers are assigned. Default is False.
+        :param first_row_header: Indicates whether the first row of the table
+            should be used as the DataFrame's header. If True, the first row
+            is used as column names; otherwise, no column headers are
+            assigned. Default is False.
         :type first_row_header: bool
 
-        :return: A pandas DataFrame object containing the desired table's data if successfully
-            extracted, or None if the table is unavailable or an error occurs during the extraction.
+        :return: A pandas DataFrame object containing the desired table's data
+            if successfully extracted, or None if the table is unavailable or
+            an error occurs during the extraction.
         :rtype: pandas.DataFrame or None
         """
 
         if not self.soup:
-         self.fetch_data()
+            self.fetch_data()
 
         # Get all tables from BeautifulSoup
         tables = self.soup.find_all('table')
 
         if len(tables) < table_number:
-            print(f"Asked for {table_number} table, but only {len(tables)} are available.")
+            print(f"Asked for {table_number} table, but only {len(tables)} "
+                  f"are available.")
             return None
 
         target_table = tables[table_number - 1]  # table_number is indexed from 1
@@ -193,34 +210,42 @@ class Scraper:
         # Use pandas.read_html( ... ) to read from target_table
         try:
             header_arg = 0 if first_row_header else None
-            html_string = str(target_table)     # convert target_table content into a string
+            html_string = str(target_table)  # convert content into a string
             html_file = io.StringIO(html_string)
-            # if a header parameter is x, then xth row becomes a columns header (if header == None, then there's no columns header)
+            # if a header parameter is x, then xth row becomes a columns
+            # header (if header == None, then there's no columns header)
             # index_col=0 treats the first column as a row header
-            dfs = pd.read_html(html_file, header=header_arg, index_col=0) # read_html needs HTML file, not a string
-            # dfs now holds objects of pandas.DataFrame type. We can convert it to CSV easily (dfs[0].to_csv("tab.csv")
+            dfs = pd.read_html(html_file, header=header_arg, index_col=0)
+            # dfs now holds objects of pandas.DataFrame type.
             if dfs:
                 return dfs[0]
             else:
                 return None
         except Exception as e:
-            print(f"Error while reading {table_number} table from {self.exact_url}: {e}")
+            print(f"Error while reading {table_number} table from "
+                  f"{self.exact_url}: {e}")
         return None
 
     def count_words(self):
         """
-        Counts the frequency of each word in the provided article (skips common page elements like a menu etc.)
-        :return: Counter of how many times each word occurred in the article or None if couldn't find content.
+        Counts the frequency of each word in the provided article (skips
+        common page elements like a menu etc.)
+        :return: Counter of how many times each word occurred in the article
+                 or None if couldn't find content.
         :rtype: Counter | None
         """
 
         if not self.soup:
-         self.fetch_data()
+            self.fetch_data()
 
-        # Count words from both the main article content and title of the article
-        title_soup = self.soup.find("h1", class_="firstHeading mw-first-heading")
-        title_text = title_soup.get_text().strip()
-        if not title_text:
+        # Count words from both the main article content and title
+        title_soup = self.soup.find("h1",
+                                    class_="firstHeading mw-first-heading")
+        if title_soup:
+            title_text = title_soup.get_text().strip()
+            if not title_text:
+                title_text = ""
+        else:
             title_text = ""
 
         main_soup = self.soup.find("div", class_="mw-parser-output")
@@ -228,16 +253,19 @@ class Scraper:
             print("Content not found.")
             return None
         main_text = main_soup.get_text().strip()
-        # Add title text to the main text and convert the resulting text to the lowercase
+        # Add title text to the main text and convert resulting text to lower
         main_text = (main_text + " " + title_text).lower()
 
         # split main_text into a list of processed words
         raw_words = main_text.split()
-        words = []  # List of all processed words of the current article that will be counted
+        # List of all processed words of the current article to be counted
+        words = []
         for word in raw_words:
             # Remove punctuation and irrelevant characters from the edges.
-            # Words having non-letter characters inside (without any blank space around non-letter character) are
-            # treated as a single word (e.g. "II/IV" is considered one word: "II/IV", "word." is processed into "word")
+            # Words having non-letter characters inside (without any blank
+            # space around non-letter character) are treated as a single word
+            # (e.g. "II/IV" is considered one word: "II/IV", "word." is
+            # processed into "word")
             cleaned = re.sub(r'^\W+|\W+$', '', word, flags=re.UNICODE)
             if cleaned:  # Only add non-empty words
                 words.append(cleaned)
--- scraping_manager_class.py
+++ scraping_manager_class.py
@@ -3,37 +3,48 @@ import os
 import time
 from collections import Counter
 from queue import Queue
+import pandas as pd
 from scraper_class import Scraper
 
 class ScrapingManager:
 
-    def __init__(self, wiki_url):
+    def __init__(self, wiki_url, use_local_html_file_instead=False):
         """
         Initialize ScrapingManager class
-        :param wiki_url: URL to the main wiki site.
+        :param wiki_url: URL to the main wiki site or path to a local file.
+        :param use_local_html_file_instead: Whether to use a local HTML file.
         """
         self.wiki_url = wiki_url
+        self.use_local_file = use_local_html_file_instead
 
     def auto_count_words(self, starting_phrase, max_depth, waiting_time=0.0):
         """
-        Automatically traverses through linked articles starting from the given phrase, counts words
-        in each article and recursively processes linked articles up to the specified maximum depth.
-        Creates or updates JSON file with counted values.
+        Automatically traverses through linked articles starting from the
+        given phrase, counts words in each article and processes linked
+        articles up to the maximum depth. Creates or updates JSON file with
+        counted values.
 
         :param starting_phrase: The initial phrase to start processing from.
         :type starting_phrase: str
-        :param max_depth: The maximum depth to traverse from the starting article.
+        :param max_depth: The maximum depth to traverse from the starting
+                          article.
         :type max_depth: int
-        :param waiting_time: number of seconds to wait between processing articles.
+        :param waiting_time: number of seconds to wait between processing
+                            articles.
         :type waiting_time: float
         :return: None
         """
+        if self.use_local_file:
+            raise ValueError("Can't use auto_count_words on a single local "
+                             "file!")
+
         visited = set()
         waiting_phrases_queue = Queue()
         waiting_phrases_queue.put((starting_phrase, 0))
 
-        # Load the initial content of the JSON file and update it as the BFS goes. At the end of this function load
-        # new content only once to the JSON file.
+        # Load the initial content of the JSON file and update it as the BFS
+        # goes. At the end of this function load new content only once to the
+        # JSON file.
         json_path = './word-counts.json'
         initial_json_content = Counter()
         if os.path.exists(json_path):
@@ -42,12 +53,15 @@ class ScrapingManager:
                     old_data = json.load(f)
                     initial_json_content.update(old_data)
             except (json.JSONDecodeError, ValueError):
-                # If a JSON file is empty or damaged, ignore its content and override it with new values.
+                # If a JSON file is empty or damaged, ignore its content and
+                # override it with new values.
                 pass
-        total_counts = initial_json_content # This value will be updated for each processed article
+        # This value will be updated for each processed article
+        total_counts = initial_json_content
 
         while not waiting_phrases_queue.empty():
-            [current_phrase, depth_of_current_phrase] = waiting_phrases_queue.get()
+            (current_phrase, depth_of_current_phrase) = \
+                waiting_phrases_queue.get()
             if current_phrase not in visited:
                 if depth_of_current_phrase <= max_depth:
                     print(f"Currently processing: {current_phrase}")
@@ -57,19 +71,26 @@ class ScrapingManager:
                     try:
                         current_scraper.fetch_data()
                     except ConnectionError as e:
-                        # If error occurred skip the subtree of this phrase and don't mark the phrase as visited.
-                        print(f"Error while fetching the data for {current_phrase}. Skipping this phrase. Error: {e}")
+                        # If error occurred skip the subtree of this phrase
+                        # and don't mark the phrase as visited.
+                        print(f"Error while fetching the data for "
+                              f"{current_phrase}. Skipping this phrase. "
+                              f"Error: {e}")
                         continue
-                    # If fetching was successful, proceed to process the article for current_phrase
+                    # If fetching was successful, proceed to process the
+                    # article for current_phrase
                     visited.add(current_phrase)
                     # Get titles of all articles linked from the current phrase
                     children_phrases = current_scraper.get_children_phrases()
                     if not children_phrases:
                         continue
                     for phrase in children_phrases:
-                        if phrase not in visited and depth_of_current_phrase < max_depth:
+                        if phrase not in visited and \
+                           depth_of_current_phrase < max_depth:
                             # Add this phrase to the BFS queue
-                            waiting_phrases_queue.put((phrase, depth_of_current_phrase + 1))
+                            waiting_phrases_queue.put(
+                                (phrase, depth_of_current_phrase + 1)
+                            )
 
                     # Get counter of current article
                     current_counts = current_scraper.count_words()
@@ -86,12 +107,47 @@ class ScrapingManager:
         # End of the BFS. Check JSON files
         try:
             with open(json_path, 'w', encoding='utf-8') as file:
-                # ensure_ascii=False is crucial for dealing with non-English characters.
+                # ensure_ascii=False is crucial for dealing with non-English
+                # characters.
                 # indent=4 makes a file easily readable.
                 json.dump(total_counts, file, indent=4, ensure_ascii=False)
         except IOError as e:
             print(f"Error while writing to {json_path}: {e}")
 
-if __name__ == '__main__':
-    s = ScrapingManager("https://bulbapedia.bulbagarden.net/wiki")
-    s.auto_count_words("Team Rocket", 1)
+    def get_table(self, phrase, table_number, first_row_header=False):
+        """
+        Extracts a specified table from a webpage or local file using a
+        scraper, saves it as a CSV file, and prints numbers of occurrences of
+        each unique value found in the table (excluding the headers)
+        :param phrase: A string used to identify and name the CSV file. All
+                       spaces in the string are replaced with underscores.
+        :type phrase: str
+        :param table_number: The index of the table to extract. The index is
+                             zero-based.
+        :type table_number: int
+        :param first_row_header: Indicates whether the first row of the table
+                                 should be used as column headers.
+        :type first_row_header: bool
+        """
+        my_scraper = Scraper(self.wiki_url, phrase, self.use_local_file)
+        df = my_scraper.get_table(table_number, first_row_header)
+        if df is not None:
+            # Write df into csv file
+            phrase = phrase.replace(" ", "_")
+            try:
+                df.to_csv(f"{phrase}.csv", index=True)
+            except Exception as e:
+                print(f"Couldn't save table to {phrase}.csv. Error : {e}")
+
+            print("--- Numbers of each values in a table ---")
+            # df.values converts pandas table into a numpy matrix
+            # (without headers)
+            # .flatten(), flatten this matrix into a single list of cells.
+            cells_list = df.values.flatten()
+            counts = pd.Series(cells_list).value_counts()
+            results_table = counts.to_frame(name="Number of occurrences")
+            print(results_table)
+        else:
+            print("Table couldn't be loaded")
+
+
